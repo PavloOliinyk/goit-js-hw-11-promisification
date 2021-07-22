8d5d2e23b67171c1bd0e9d36dda75f1b86599376
Pavlo Oliinyk <oliinyk.pavlo@gmail.com>
2021-07-22 16:58:04 +0300

    update task-3

--- src/js/task-3.js
+++ src/js/task-3.js
@@ -1,45 +1,41 @@
-// const randomIntegerFromInterval = (min, max) => {
-//   return Math.floor(Math.random() * (max - min + 1) + min);
-// };
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
 
-// const makeTransaction = (transaction, onSuccess, onError) => {
-//   const delay = randomIntegerFromInterval(200, 500);
+const makeTransaction = (transaction) => {
+  const delay = randomIntegerFromInterval(200, 500);
+  const canProcess = Math.random() > 0.3;
 
-//   setTimeout(() => {
-//     const canProcess = Math.random() > 0.3;
+  if (canProcess) {
+    return Promise.resolve(transaction.id, delay);
+  }
 
-//     if (canProcess) {
-//       onSuccess(transaction.id, delay);
-//     } else {
-//       onError(transaction.id);
-//     }
-//   }, delay);
-// };
+  return Promise.reject(transaction.id);
+};
 
-// const logSuccess = (id, time) => {
-//   console.log(`Transaction ${id} processed in ${time}ms`);
-// };
+const logSuccess = (id, time) => {
+  console.log(`Transaction ${id} processed in ${time}ms`);
+};
 
-// const logError = (id) => {
-//   console.warn(`Error processing transaction ${id}. Please try again later.`);
-// };
+const logError = (id) => {
+  console.warn(`Error processing transaction ${id}. Please try again later.`);
+};
 
-// /*
-//  * Работает так
-//  */
+/*
+ * Работает так
+ */
 // makeTransaction({ id: 70, amount: 150 }, logSuccess, logError);
 // makeTransaction({ id: 71, amount: 230 }, logSuccess, logError);
 // makeTransaction({ id: 72, amount: 75 }, logSuccess, logError);
 // makeTransaction({ id: 73, amount: 100 }, logSuccess, logError);
-// /*
-//  * Должно работать так
-//  */
-// makeTransaction({ id: 70, amount: 150 }).then(logSuccess).catch(logError);
+/*
+ * Должно работать так
+ */
+makeTransaction({ id: 70, amount: 150 }).then(logSuccess).catch(logError);
 
-// makeTransaction({ id: 71, amount: 230 }).then(logSuccess).catch(logError);
+makeTransaction({ id: 71, amount: 230 }).then(logSuccess).catch(logError);
 
-// makeTransaction({ id: 72, amount: 75 }).then(logSuccess).catch(logError);
+makeTransaction({ id: 72, amount: 75 }).then(logSuccess).catch(logError);
 
-// makeTransaction({ id: 73, amount: 100 }).then(logSuccess).catch(logError);
+makeTransaction({ id: 73, amount: 100 }).then(logSuccess).catch(logError);
 
-console.log('Вызов таск №3');
